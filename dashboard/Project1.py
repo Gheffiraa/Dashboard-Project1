ea33c1396787e5d03e49d5a3cc7d6d88997eb199
unknown <gheffira.tsuraya1003@mail.ugm.ac.id>
2024-03-06 02:16:25 +0700

    cape

--- dashboard/Project1.py
+++ dashboard/Project1.py
@@ -4,31 +4,26 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotly.express as px
+import os
 
+st.title('Average Concentration of PM2.5 Pollutant :sparkles:')
 path = os.path.dirname(__file__)
 full_path = path + '/data1.csv'
-df = pd.read_csv(full_path)
- 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
- 
-colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
- 
-sns.barplot(x="PM2.5", y="station", data=data1(5), palette=colors, ax=ax[0])
-ax[0].set_ylabel(None)
-ax[0].set_xlabel("Concentration PM2.5", fontsize=30)
-ax[0].set_title("Highest PM2.5 Polluted Station", loc="center", fontsize=50)
-ax[0].tick_params(axis='y', labelsize=35)
-ax[0].tick_params(axis='x', labelsize=30)
- 
-sns.barplot(x="PM2.5", y="station", data=data1.sort_values(by="PM2.5", ascending=True).head(5), palette=colors, ax=ax[1])
-ax[1].set_ylabel(None)
-ax[1].set_xlabel("Concentration PM2.5", fontsize=30)
-ax[1].invert_xaxis()
-ax[1].yaxis.set_label_position("right")
-ax[1].yaxis.tick_right()
-ax[1].set_title("Lowest PM2.5 Polluted Station", loc="center", fontsize=50)
-ax[1].tick_params(axis='y', labelsize=35)
-ax[1].tick_params(axis='x', labelsize=30)
- 
+data1 = pd.read_csv(full_path)
+
+fig, ax = plt.subplots(figsize = (15, 7))
+
+ax = sns.barplot(data = data1, x='station', y='PM2.5', dodge = False, ax = ax, palette = 'turbo')
+ax.set_title('Average PM2.5 concentration Grouped by Station (Hourly Recorded)',fontsize=16)
+ax.set_xlabel('Station')
+ax.set_ylabel('PM 2.5 Average Concentration')
 st.pyplot(fig)
+
+wind = data1[['wd','PM2.5']].groupby('wd').median()
+fig2, ax2 = plt.subplots(figsize = (15, 7))
+# plot
+ax2 = sns.barplot(data = wind, x = 'wd', y = 'PM2.5', palette = 'turbo')
+ax2.set_title('Average PM2.5 concentration in Dongsi Air Grouped by Wind Direction (Hourly Recorded)',fontsize=16)
+ax2.set_xlabel('Wind Direction')
+ax2.set_ylabel('PM2.5 Concentration')
+st.pyplot(fig2)
